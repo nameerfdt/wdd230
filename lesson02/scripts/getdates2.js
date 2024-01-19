f4a883e9ff177fb10718f92a0b1a9b0ae92bbbad
nameerfdt <nameerfdt@byui.edu>
2024-01-18 21:14:44 -0800

    corrected getdates spelling error

--- lesson02/scripts/getdates2.js
+++ lesson02/scripts/getdates2.js
@@ -1,5 +1,5 @@
-let test = document.lastModified;
-document.getElementById("lastmodification").innerHTML = text;
-
 let currentyear = new Date().getFullYear();
 document.getElementById("currentyear").innerHTML = currentyear;
+
+let text = document.lastModified;
+document.getElementById("lastmodification").innerHTML = text;

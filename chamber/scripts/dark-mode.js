1859b74ca964f15adae69eed1a969ad00bf5f28f
nameerfdt <nameerfdt@byui.edu>
2024-02-06 20:28:50 -0800

    dark mode updates

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,6 +1,7 @@
 const modeButton = document.querySelector("#dark-mode");
 const body = document.querySelector("body");
-const main = document.querySelector("main");
+const main = document.querySelector("main > div");
+
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle("dark-mode");

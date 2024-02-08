81cbb55f1eb022e041318722fd3ee88c1db6a83b
nameerfdt <nameerfdt@byui.edu>
2024-02-07 17:37:39 -0800

    dark mode attempts

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,9 +1,16 @@
 const modeButton = document.querySelector("#dark-mode");
 const body = document.querySelector("body");
-const main = document.querySelector("main > div");
+const main = document.querySelector("main");
+// const section = document.querySelector("section");
+const weather = document.querySelector("#weather");
+
+// const mainsection = document.querySelector("main > section");
 
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
     main.classList.toggle("dark-mode");
+    section.classList.toggle("dark-mode");
+    weather.classList.toggle("dark-mode");
+    // mainsection.classList.toggle("dark-mode");
 });

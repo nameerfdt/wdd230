e120f5b8e1bf77232fbd518293b69c0ae16fed36
nameerfdt <nameerfdt@byui.edu>
2024-02-08 18:46:01 -0800

    added darkmode js script

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,16 +1,30 @@
-const modeButton = document.querySelector("#dark-mode");
-const body = document.querySelector("body");
-const main = document.querySelector("main");
-// const section = document.querySelector("section");
-const weather = document.querySelector("#weather");
+// const modeButton = document.querySelector("#dark-mode");
+// const body = document.querySelector("body");
+// const main = document.querySelector("main");
+// // const events = document.querySelector ('#events');
+// // const weather = document.querySelector('#weather');
+// // const spotlights = document.querySelector('.chamber-member');
+// // const header = document.querySelector('#hero-box');
 
-// const mainsection = document.querySelector("main > section");
+
+// modeButton.addEventListener("click", () => {
+//     body.classList.toggle("dark-mode");
+//     main.classList.toggle("dark-mode");
+// });
 
 
-modeButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-    main.classList.toggle("dark-mode");
-    section.classList.toggle("dark-mode");
-    weather.classList.toggle("dark-mode");
-    // mainsection.classList.toggle("dark-mode");
-});
+const mainElement = document.querySelector('main');
+// Select all elements within the main element
+const allElementsInMain = mainElement.querySelectorAll('*');
+
+// Iterate over each element within the main element using forEach
+allElementsInMain.forEach(element => {
+    // Change the background color of each element
+    element.style.backgroundColor = 'black';
+	element.style.color = 'white';
+	});
+
+
+
+
+

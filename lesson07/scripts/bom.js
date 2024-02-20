10ec5cdc4cc4404c7d0708e82eaabec61413ea25
nameerfdt <nameerfdt@byui.edu>
2024-02-20 13:28:42 -0800

    updates to js for local storage

--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -5,66 +5,13 @@ const list = document.querySelector('#list');
 
 // Declare an array named chaptersArray and assign it to the results of a defined function named getChapterList or an empty array if the function call returns null or undefined
 let chaptersArray = getChapterList() || [];
-
-// // click event listener for the Add Chapter button using addEventListener and an anonymous function or arrow function.
-// button.addEventListener('click', () => {
-//     // check to make sure the input is not blank
-//     if (input.value != '') {
-//         // create a li element
-//         let li = document.createElement('li');
-//         // create a deliete button
-//         let deleteButton = document.createElement('button');
-//         // create variable for input value
-//         let word = input.value;
-//         //  create variable to uppercase first letter and lowercase all others
-//         let capWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-//         // populate the li elements textContent or innerHTML with the input value
-//         li.textContent = capWord;
-//         // populate the button textContent with a ❌
-//         deleteButton.textContent = '❌';
-//         // append the li element with the delete button
-//         li.append(deleteButton);
-//         // append the li element to the unordered list in your HTML
-//         list.append(li);
-//         // set input value back to null
-//         input.value = '';
-//         // send the focus to the input element
-//         input.focus();
-//             // add an event listener to the delete button that removes the li element when clicked
-//             deleteButton.addEventListener('click', function (){
-//                 list.removeChild(li);
-//                 input.focus();
-//         input.value = '';
-//             })
-//         }
-//     });
     
-// // function to allow Enter key to add input to list
-// input.addEventListener('keydown', function (event){
-//     if (event.key === 'Enter'){
-//         event.preventDefault();
-//         let li = document.createElement('li');
-//         let deleteButton = document.createElement('button');
-//         deleteButton.textContent = '❌';
-//         deleteButton.addEventListener('click', function (){
-//             list.removeChild(li);
-//             input.focus();
-//             input.value = '';
-//         })    
-//         let word = input.value;
-//         let capWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-//         li.textContent = capWord;
-//         li.appendChild(deleteButton);
-//         list.append(li);
-//         input.value = '';
-//     }
-// });
-
 // Now let's populate the displayed list of chapters. Use a forEach on the chaptersArray to process each entry named chapter. Use an arrow function within the loop to call a new defined function named displayList and pass it the argument of chapter. That way each entry will be processed, i.e., appended to the list.
 chaptersArray.forEach(chapter => {
     displayList(chapter);
 });
 
+// create event listener when button is clicked
 button.addEventListener('click', () => {
     // makes sure input value is not empty
     if (input.value != '') {
@@ -81,17 +28,16 @@ button.addEventListener('click', () => {
     }
 });
 
-// Create the displayList defined function that receives one parameter named item.
+// Create the displayList defined function that receives one parameter named item. The item parameter is the input.
 function displayList(item) {
-    // create a li element
+    // create a li (list) element
     let li = document.createElement('li');
     // create a deliete button
     let deleteButton = document.createElement('button');
-    // create variable for input value
-    // let word = input.value;
-    //  create variable to uppercase first letter and lowercase all others
-    // let capWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    // populate the li elements textContent or innerHTML with the input value
+    // let word = input.value; not needed because item is input
+    //  create variable to uppercase first letter and lowercase all others from the input (item)
+    // let capWord = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+    // populate the list elements textContent or innerHTML with the input value. the input value is item
     li.textContent = item;
     // populate the button textContent with a ❌
     deleteButton.textContent = '❌';
@@ -105,7 +51,7 @@ function displayList(item) {
     // input.value = '';
     // send the focus to the input element
     // input.focus();
-        // add an event listener to the delete button that removes the li element when clicked
+    // add an event listener to the delete button that removes the li element when clicked
     deleteButton.addEventListener('click', function (){
         list.removeChild(li);
         // removes the chapter from the array and local storage

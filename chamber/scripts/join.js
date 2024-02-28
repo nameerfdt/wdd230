1a4cf4277235430f52a2d8d7479e4b5df109c17e
nameerfdt <nameerfdt@byui.edu>
2024-02-27 21:20:41 -0800

    added hidden and corrected email requirements

---
+++ chamber/scripts/join.js
@@ -0,0 +1,4 @@
+// get current date and time in milliseconds
+let currentDateTime = Date.now();
+// put the current date and time in html
+document.getElementById('currentDateTime').value = currentDate;

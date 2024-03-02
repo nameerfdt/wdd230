e70cb62120379c5c6d7131fd37710041c33e2a86
nameerfdt <nameerfdt@byui.edu>
2024-03-02 09:54:51 -0800

    commented out unused darkmode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,20 +1,20 @@
 const modeButton = document.querySelector("#darkmodeBtn");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
-const events = document.querySelector("#events");
-const weather = document.querySelector("#weather");
-const spot1 = document.querySelector("#spotlight1");
-const spot2 = document.querySelector("#spotlight2");
-const spot3 = document.querySelector("#spotlight3");
+// const events = document.querySelector("#events");
+// const weather = document.querySelector("#weather");
+// const spot1 = document.querySelector("#spotlight1");
+// const spot2 = document.querySelector("#spotlight2");
+// const spot3 = document.querySelector("#spotlight3");
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle('darkmode');
     main.classList.toggle('darkmode');
-    events.classList.toggle('darkmode');
-    weather.classList.toggle('darkmode');
-    spot1.classList.toggle('darkmode');
-    spot2.classList.toggle('darkmode');
-    spot3.classList.toggle('darkmode');
+    // events.classList.toggle('darkmode');
+    // weather.classList.toggle('darkmode');
+    // spot1.classList.toggle('darkmode');
+    // spot2.classList.toggle('darkmode');
+    // spot3.classList.toggle('darkmode');
 });
 
 

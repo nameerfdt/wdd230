ef2c7c7e617fc47c6b8007e5651e00bcab5a8d29
nameerfdt <nameerfdt@byui.edu>
2024-03-07 19:40:54 -0800

    updates to member js

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -8,3 +8,45 @@ async function getMemberData() {
     displayMembers(data.members);
 }
     getMemberData();
+
+const memberCards = document.getElementById('memberCards');
+
+const displayMembers = (members) => {
+
+    members.forEach((member) => {
+        const card = document.createElement('section');
+        const businessName = document.createElement('h3');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const website = document.createElement('a');
+
+        const membership_level = document.createElement('p');
+        const other_information = document.createElement('p');
+        const logo = document.createElement('img');
+
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '200');
+        logo.setAttribute('width', '200');
+        businessName.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phone}`;
+        website.textContent = `${member.website}`;
+        membership_level.textContent = `${member.membership_level}`;
+        other_information.textContent = `${member.other_information}`;
+        
+        card.appendChild(logo);
+        card.appendChild(businessName);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(membership_level);
+        card.appendChild(other_information);
+        
+        
+
+    memberCards.appendChild(card);
+    });
+    
+}
+

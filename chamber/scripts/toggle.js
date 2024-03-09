5bffd6757a07221ddfdf7db68aceea6efbb5c84b
nameerfdt <nameerfdt@byui.edu>
2024-03-08 17:51:38 -0800

    added toggle js

---
+++ chamber/scripts/toggle.js
@@ -0,0 +1,14 @@
+
+const gridBtn = document.querySelector("#gridBtn");
+const listBtn = document.querySelector('#listBtn');
+const display = document.querySelector('article');
+
+gridBtn.addEventListener("click", () => {
+    display.classList.add('gridBtn');
+    display.classList.remove('listBtn');
+});
+
+listBtn.addEventListener("click", () => {
+    display.classList.add('listBtn');
+    display.classList.remove('gridBtn');
+});

9b09503fb3767c063bc8f5db57f57f14c54e17a6
nameerfdt <nameerfdt@byui.edu>
2024-03-09 14:23:56 -0800

    changed image size

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -33,7 +33,7 @@ const displayMembers = (members) => {
         logo.setAttribute('src', member.image);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '150');
-        logo.setAttribute('width', '150');
+        logo.setAttribute('height', 'auto');
         businessName.textContent = `${member.name}`;
         // businessName.style.setProperty('color', 'black');
         address.textContent = `${member.address}`;

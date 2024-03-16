6cdd975d5f464e6a642d348f40855d3286996b42
nameerfdt <nameerfdt@byui.edu>
2024-03-15 19:58:34 -0700

    windchill calulation

--- chamber/scripts/weather1.js
+++ chamber/scripts/weather1.js
@@ -48,37 +48,20 @@ function displayResults(today){
         windSpeed.textContent = `${today.wind.speed.toFixed(0)}`;
         
 
-        // function calculateWindChill(t, w) {
-        //     if (t <= 50 && w > 3.0) {
-        //     const windchill = 34.74 + (0.6215 * t) - (35.75 * w ** 0.16) + (0.4275 * t * (w ** 0.16))
-        //     return windchill.toFixed(2);
-        //     } else {
-        //         return "N/A";
-        //     }
-        // }
+        function calculateWindChill(currentTemp, windspeed) {
+            if (currentTemp <= 50 && windspeed > 3.0) {
+                const windchill = 34.74 + (0.6215 * currentTemp) - (35.75 * windspeed ** 0.16) + (0.4275 * currentTemp * (windspeed ** 0.16))
+                return windchill.toFixed(2);
+            } else {
+                return "N/A";
+            }
+        }
+        const windchill = calculateWindChill(today.main.temp, today.wind.speed);
+        document.getElementById("windChill").innerText = windchill;
+}
         
-        // const windchill = calculateWindChill(t, w);
-        
-        // document.getElementById("windChill").innerText = windchill;
-
 function capitalize(str) {
     return str.replace(/\b\w/g, function(char){
         return char.toUpperCase();
     })
 }
-}
-
-
-
-// function calculateWindChill(t, w) {
-//     if (t <= 50 && w > 3.0) {
-//     const windchill = 34.74 + (0.6215 * t) - (35.75 * w ** 0.16) + (0.4275 * t * (w ** 0.16))
-//     return windchill.toFixed(2);
-//     } else {
-//         return "N/A";
-//     }
-// }
-
-// const windchill = calculateWindChill(t, w);
-
-// document.getElementById("windChill").innerText = windchill;

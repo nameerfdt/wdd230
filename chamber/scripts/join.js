f6274ac39f7daff689754846fe25dd43be74a42a
nameerfdt <nameerfdt@byui.edu>
2024-03-20 19:27:02 -0700

    removed commented out lines

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -4,5 +4,4 @@ let currentTimestamp = document.querySelector('#timeStamp');
 // set currentTimestamp to current date
 currentTimestamp.value = Date.now();
 
-// console.log(currentTimestamp);
 
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,9 +1,6 @@
 let t = parseFloat(document.getElementById("temperature").innerText);
 let w = parseFloat(document.getElementById("windSpeed").innerText);
 
-// let t = 40;
-// let w = 15;
-
 function calculateWindChill(t, w) {
     if (t <= 50 && w > 3.0) {
     const windchill = 34.74 + (0.6215 * t) - (35.75 * w ** 0.16) + (0.4275 * t * (w ** 0.16))
